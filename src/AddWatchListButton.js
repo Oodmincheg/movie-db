56ca162e2cdc83f5c607dc8a8c29dc23c13a691e
Oodmincheg <Vlad Vorobyov>
2018-12-19 15:12:29 +0200

    fix button with localstorage(i think...)

--- src/AddWatchListButton.js
+++ src/AddWatchListButton.js
@@ -4,17 +4,35 @@ class WatchListButton extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      inWatchList: Boolean(this.props.id in localStorage)
+      watchlist: {},
+      inWatchList: false
     };
     this.deleteFromWatchList = this.deleteFromWatchList.bind(this);
     this.addToWatchList = this.addToWatchList.bind(this);
   }
+  componentDidMount = () => {
+    if (localStorage.getItem("watchlist")) {
+      let watchlist = JSON.parse(localStorage.getItem("watchlist"));
+      this.setState({ watchlist });
+      this.setState({ inWatchList: Boolean(this.props.id in watchlist) });
+    } else {
+      localStorage.setItem("watchlist", "{}");
+    }
+  };
+  componentWillUnmount = () => {
+    let watchlist = JSON.parse(localStorage.getItem("watchlist"));
+    if (this.state.inWatchList && !(this.props.id in watchlist)) {
+      watchlist[this.props.id] = this.props.title;
+    }
+    if (!this.state.inWatchList && this.props.id in watchlist) {
+      delete watchlist[this.props.id];
+    }
+    localStorage.setItem("watchlist", JSON.stringify(watchlist));
+  };
   deleteFromWatchList() {
-    localStorage.removeItem(this.props.id);
     this.setState({ inWatchList: false });
   }
   addToWatchList() {
-    localStorage.setItem(this.props.id, this.props.title);
     this.setState({ inWatchList: true });
   }
   render() {
