1b8586ff5f0585bf3cd03a0fcff27aa81d185ec6
Oodmincheg <Vlad Vorobyov>
2018-12-25 17:11:43 +0200

    start try version with redux(begin rootReducer)

---
+++ src/reducers.js
@@ -0,0 +1,12 @@
+const DEFAULT_STATE = {
+  searchString: ""
+};
+
+const rootReducer = (state = DEFAULT_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default rootReducer;
