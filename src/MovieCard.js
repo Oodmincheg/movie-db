f7865f3c6c90235ab28c91ca87c1bddce2ee5e37
Oodmincheg <Vlad Vorobyov>
2019-01-26 19:11:08 +0200

    finish styling

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -1,16 +1,38 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import styled from "styled-components";
-import WatchListButton from "./AddWatchListButton";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import WatchListButton from './AddWatchListButton';
 
 const Wrapper = styled.div`
+  font-family: 'Inconsolata', monospace;
   width: 40%;
   margin: 0 4% 20px 4%;
-  border: 2px solid #333;
+  border: 2px solid lightblue;
   border-radius: 4px;
+  text-align: center;
+  font-style: italic;
 `;
 const Image = styled.img`
+  border: 2px solid lightblue;
   float: left;
+  margin-right: 20px;
+`;
+
+const Genre = styled.div`
+  &:before {
+    content: 'Genre: ';
+    font-weight: bold;
+    color: darkblue;
+  }
+  margin: 30px auto;
+`;
+
+const H2 = styled.h2`
+  color: darkblue;
+  &:visited {
+    color: blue;
+  }
+  text-decoration: none;
 `;
 const MovieCard = props => {
   const { id, title, genre_ids } = props.movie;
@@ -18,16 +40,17 @@ const MovieCard = props => {
   return (
     <Wrapper className="movie-card">
       <Image src={imgSrc} alt={title} />
-      <Link to={`/movie/${id}`}>
-        <h2>{title}</h2>
+      <Link to={`/movie/${id}`} style={{ 'text-decoration': 'none' }}>
+        <H2>{title}</H2>
       </Link>
-      <WatchListButton id={id} title={title} />
-      <p>
+
+      <Genre>
         {props.allGenres
           .filter(genre => genre_ids.includes(genre.id))
           .map(genre => genre.name)
-          .join(", ")}
-      </p>
+          .join(', ')}
+      </Genre>
+      <WatchListButton id={id} title={title} />
     </Wrapper>
   );
 };
--- src/WatchList.js
+++ src/WatchList.js
@@ -1,6 +1,29 @@
-import React from "react";
-import WatchListButton from "./AddWatchListButton";
+import React from 'react';
+import WatchListButton from './AddWatchListButton';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
+const Wraper = styled.div`
+  padding-left: 20px;
+  padding-right: 20px;
+  border-radius: 5px;
+  border: 2px lightblue solid;
+  margin: 0 auto;
+  max-width: 22%;
+  & h2 {
+    color: darkblue;
+    & a {
+      font-size: 12px;
+      margin-left: 5px;
+    }
+  }
+`;
+const LI = styled.li`
+  color: darkblue;
+  margin: 10px auto;
+  display: flex;
+  justify-content: space-between;
+`;
 class WatchList extends React.Component {
   constructor(props) {
     super(props);
@@ -9,23 +32,29 @@ class WatchList extends React.Component {
     };
   }
   componentDidMount() {
-    let watchList = JSON.parse(localStorage.getItem("watchlist"));
+    let watchList = JSON.parse(localStorage.getItem('watchlist'));
     this.setState({ watchList });
     console.log(this.state.watchList);
   }
   render() {
-    if (JSON.stringify(this.state.watchList) == "{}") {
+    if (JSON.stringify(this.state.watchList) == '{}') {
       return <h2>Watch list is empty</h2>;
     } else {
       return (
-        <ul>
-          {Object.keys(this.state.watchList).map(id => (
-            <li key={id}>
-              {this.state.watchList[id]}{" "}
-              <WatchListButton id={id} title={this.state.watchList.id} />{" "}
-            </li>
-          ))}
-        </ul>
+        <Wraper>
+          <h2>
+            Watch list
+            <Link to="/">(main page)</Link>
+          </h2>
+          <ul>
+            {Object.keys(this.state.watchList).map(id => (
+              <LI key={id}>
+                {this.state.watchList[id]}{' '}
+                <WatchListButton id={id} title={this.state.watchList.id} />{' '}
+              </LI>
+            ))}
+          </ul>
+        </Wraper>
       );
     }
   }
